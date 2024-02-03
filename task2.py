71088719e03fd2203554d9bb0fbd6a7eebe226f1
Andanchenko Maria <andanchenko.maria@gmail.com>
2024-02-03 21:59:15 +0200

    Update task2.py

--- task2.py
+++ task2.py
@@ -5,7 +5,7 @@ import scipy.integrate as spi
 
 # Визначення функції та межі інтегрування
 def f(x):
-    return x ** 2
+    return x**2
 
 
 def monte_carlo_integrate(func, a, b, y_min, y_max, num_points):
@@ -26,13 +26,22 @@ if __name__ == "__main__":
     print("Інтеграл, визначений за допомогою quad: ", result, error)
 
     mc_result = monte_carlo_integrate(f, a, b, 0, 4, 100_000)
-    print("Інтеграл, визначений за допомогою методу Монте-Карло з 100_000 випробуваннями: ", mc_result)
+    print(
+        "Інтеграл, визначений за допомогою методу Монте-Карло з 100_000 випробуваннями: ",
+        mc_result,
+    )
 
     mc_result2 = monte_carlo_integrate(f, a, b, 0, 4, 1_000_000)
-    print("Інтеграл, визначений за допомогою методу Монте-Карло з 1_000_000 випробуваннями: ", mc_result2)
+    print(
+        "Інтеграл, визначений за допомогою методу Монте-Карло з 1_000_000 випробуваннями: ",
+        mc_result2,
+    )
 
     mc_result3 = monte_carlo_integrate(f, a, b, 0, 4, 10_000_000)
-    print("Інтеграл, визначений за допомогою методу Монте-Карло з 10_000_000 випробуваннями: ", mc_result3)
+    print(
+        "Інтеграл, визначений за допомогою методу Монте-Карло з 10_000_000 випробуваннями: ",
+        mc_result3,
+    )
 
     # Створення діапазону значень для x
     x = np.linspace(-0.5, 2.5, 400)
@@ -42,23 +51,22 @@ if __name__ == "__main__":
     fig, ax = plt.subplots()
 
     # Малювання функції
-    ax.plot(x, y, 'r', linewidth=2)
+    ax.plot(x, y, "r", linewidth=2)
 
     # Заповнення області під кривою
     ix = np.linspace(a, b)
     iy = f(ix)
-    ax.fill_between(ix, iy, color='gray', alpha=0.3)
+    ax.fill_between(ix, iy, color="gray", alpha=0.3)
 
     # Налаштування графіка
     ax.set_xlim([x[0], x[-1]])
     ax.set_ylim([0, max(y) + 0.1])
-    ax.set_xlabel('x')
-    ax.set_ylabel('f(x)')
+    ax.set_xlabel("x")
+    ax.set_ylabel("f(x)")
 
     # Додавання меж інтегрування та назви графіка
-    ax.axvline(x=a, color='gray', linestyle='--')
-    ax.axvline(x=b, color='gray', linestyle='--')
-    ax.set_title('Графік інтегрування f(x) = x^2 від ' + str(a) + ' до ' + str(b))
+    ax.axvline(x=a, color="gray", linestyle="--")
+    ax.axvline(x=b, color="gray", linestyle="--")
+    ax.set_title("Графік інтегрування f(x) = x^2 від " + str(a) + " до " + str(b))
     plt.grid()
     plt.show()
-
